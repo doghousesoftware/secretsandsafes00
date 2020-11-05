4dea1166e081a678d075a7d696320024a15b3c65
bryan weinstein <bryanweinstein@outlook.com>
2020-11-05 15:49:15 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 2) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.StickFigure)
+        music.playMelody("C5 C C5 C C5 C C5 C ", 120)
+    }
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
     basic.showString("Safe")
